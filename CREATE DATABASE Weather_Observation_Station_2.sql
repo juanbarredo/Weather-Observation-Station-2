6d8ef000674b5abf41d656288549e7da20cfbe96
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2024-12-03 18:52:57 -0500

    12 03 2024
    Having to spend a lot of time cleaning the STATION.CSV
    
    I thought I would have been able to use the import export wizard like before but the address field in the STATION table has to be clarified with commas.
    
    Previously on other problems using the space delimiter on the import export wizard was enough
    
    Thankfully, there are only 499 rows.
    
    This will probably take me a few pomodori.  A pomodoro is an uninterrupted 25 minute block of focused study time.

---
+++ CREATE DATABASE Weather_Observation_Station_2.sql
@@ -0,0 +1,6 @@
+USE master;
+CREATE DATABASE Weather_Observation_Station_2;
+
+--Here I am creating the database that I will use
+--to solve the problem on hackerrank.com titled:
+--"Weather Observation Station 2"
---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather-Observation-Station-2
+ Problem on hackerrank.com titled "Weather Observation Station 2"
